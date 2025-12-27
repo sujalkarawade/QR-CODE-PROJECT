d3052a7f8db8051f3046233e9794ed08143c3ac8
sujalkarawade <sujalkarawade18@gmail.com>
2025-12-27 19:17:04 +0530

    ui changed

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
-import inquirer from "inquirer";
+import express from "express";
 import qr from "qr-image";
 import fs from "fs";
+import path from "path";
+import open from "open";
 
+const app = express();
+const PORT = 3000;
 
-inquirer
-  .prompt([
-    {
-      message: "Type in your URL: ",
-      name: "URL",
-    },
-  ])
-  .then((answers) => {
-    const url = answers.URL;
-    var qr_svg = qr.image(url);
-    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-    fs.writeFile("URL.txt", url, (err) => {
-      if (err) throw err;
-      console.log("The file has been saved!");
-    });
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-    } else {
-    }
-    });
+// Home page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "public", "index.html"));
+});
+
+// Generate QR
+app.post("/generate", (req, res) => {
+  const url = req.body.url;
+
+  const qr_svg = qr.image(url);
+  const qrPath = "public/qr_img.png";
+  qr_svg.pipe(fs.createWriteStream(qrPath));
+
+  fs.writeFile("URL.txt", url, (err) => {
+    if (err) throw err;
+  });
+
+  res.send(`
+    <h2>QR Code Generated</h2>
+    <img src="/qr_img.png" />
+    <br><br>
+    <a href="/">Generate Another</a>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+  open(`http://localhost:${PORT}`);
+});
